cdff0d2cc378baab9aad50dd6abcde24bae9a6fa
Hamcha <zikyky@gmail.com>
2017-10-27 18:38:07 +0200

    Get rid of the stupid space cadet

--- keymap.c
+++ keymap.c
@@ -25,8 +25,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		{ KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC },
 		{ KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS },
 		{ ESCFN  , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, xxxxxxx, KC_ENT  },
-		{ KC_LSPO, xxxxxxx, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, xxxxxxx, KC_RSFT },
-		{ KC_LCTL, KLOCK  , KC_LALT, xxxxxxx, xxxxxxx, KC_SPC , xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, MO(LFN), KC_RALT, KC_RGUI, KC_RCTL }
+		{ KC_LSFT, xxxxxxx, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, xxxxxxx, KC_UP   },
+		{ KC_LCTL, KLOCK  , KC_LALT, xxxxxxx, xxxxxxx, KC_SPC , xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, MO(LFN), KC_LEFT, KC_DOWN, KC_RGHT }
 	},
 	/* 1: fn */
 	[LFN] = {
@@ -41,8 +41,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		{ _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
 		{ _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
 		{ _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, xxxxxxx, _______ },
-		{ _______, xxxxxxx, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, xxxxxxx, KC_UP   },
-		{ _______, _______, _______, xxxxxxx, xxxxxxx, _______, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, _______, KC_LEFT, KC_DOWN, KC_RGHT }
+		{ _______, xxxxxxx, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, xxxxxxx, KC_RSFT },
+		{ _______, _______, _______, xxxxxxx, xxxxxxx, _______, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, _______, KC_RALT, KC_RGUI, KC_RCTL }
 	}
 };
 
