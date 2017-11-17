5c94df01a813e17a79521510cd061620b628a1cd
Hamcha <zikyky@gmail.com>
2017-11-17 12:33:33 +0100

    Overlay alternative arrow key layout on FN

--- keymap.c
+++ keymap.c
@@ -36,10 +36,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		{ KC_GRV , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_DEL  },
 		{ KC_CAPS, _______, KC_UP  , _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, KC_INS  },
 		{ _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGUP, _______, _______ },
-		{ _______, xxxxxxx, _______, _______, _______, _______, _______, _______, _______, _______, KC_END , KC_PGDN, _______, _______ },
-		{ _______, CPSCRN , _______, xxxxxxx, xxxxxxx, TGARR  , xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, _______, _______, _______, _______ }
+		{ _______, xxxxxxx, _______, _______, _______, _______, _______, _______, _______, _______, KC_END , KC_PGDN, _______, KC_RSFT },
+		{ _______, CPSCRN , _______, xxxxxxx, xxxxxxx, TGARR  , xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, _______, KC_RALT, KC_RGUI, KC_RCTL }
 	},
-	/* 2: arrows overlay */
+	/* 2: non-arrow overlay */
 	[LARR] = {
 		{ _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
 		{ _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
