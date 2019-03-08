562f358a2b8841e7cd57d7470902f744558ef347
‮ittaG ordnasselA <zikyky@gmail.com>
2019-03-08 16:19:01 +0100

    Fix for running on DZ60 + RGB keys

--- keymap.c
+++ keymap.c
@@ -1,5 +1,4 @@
-#include "gh60.h"
-#include "action_layer.h"
+#include QMK_KEYBOARD_H
 
 #define _______ KC_TRNS
 #define xxxxxxx KC_NO
@@ -38,8 +37,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[LFN] = {
 		{ KC_GRV , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_DEL  },
 		{ KC_CAPS, _______, KC_UP  , _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, KC_INS  },
-		{ _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_HOME, KC_PGUP, _______, _______ },
-		{ _______, xxxxxxx, _______, _______, _______, _______, _______, _______, _______, _______, KC_END , KC_PGDN, _______, KC_RSFT },
+		{ _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_HOME, KC_PGUP, _______, RESET   },
+		{ _______, xxxxxxx, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_END , KC_PGDN, _______, KC_RSFT },
 		{ _______, CPSCRN , _______, xxxxxxx, xxxxxxx, TGARR  , xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, _______, KC_RALT, KC_RGUI, KC_RCTL }
 	},
 	/* 2: non-arrow overlay */
